36d2e9fc372a7a7b314edcec264bc342d3d0a75f
lorcai <126689730+lorcai@users.noreply.github.com>
2024-09-03 15:17:17 +0200

    Update loops.sh

--- loops.sh
+++ loops.sh
@@ -1,5 +1,6 @@
 # Just stock loops
 ##############################################################
+https://stackoverflow.com/questions/8880603/loop-through-an-array-of-strings-in-bash
 
 # Basic
 ## declare an array variable
